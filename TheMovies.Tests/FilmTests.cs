3294af7d965ff25602e3f49f60a100e86958ab02
Soren <dasurfas@gmail.com>
2026-09-03 00:10:10 -0700

    Unit tests paa Film.cs

---
+++ TheMovies.Tests/FilmTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheMovies.Core.Models;
+
+namespace TheMovies.Tests
+{
+    // Test klasse.
+    [TestClass]
+    public class FilmTests
+    {
+        [TestMethod]
+        public void Constructor_MedTreArgumenter_SaetterTitelGenreVarighed()
+        {
+            // Arrange + Act (Opret objektet):
+            Film film = new Film("Alien", "Sci-fi", 120);
+
+            // Assert (Forventet foerst, faktisk bagefter):
+            Assert.AreEqual("Alien", film.Titel);
+            Assert.AreEqual("Sci-fi", film.Genre);
+            Assert.AreEqual(120, film.Varighed);
+        }
+
+        [TestMethod]
+        public void Constructor_UdenValgfrieArgumenter_GiverDefaultVaerdier()
+        {
+            // Arrange + Act:
+            Film film = new Film("Alien", "Sci-fi", 120);
+
+            // Assert: De 2 valgfrie parametre's defaults:
+            Assert.AreEqual("", film.Instruktør);
+            Assert.AreEqual(default(DateTime), film.PremiereDato);
+        }
+
+        [TestMethod]
+        public void Constructor_MedAlleArgumenter_SaetterInstruktoerOgPremiereDato()
+        {
+            // Arrange:
+            DateTime premiere = new DateTime(1979, 5, 25);
+
+            // Act:
+            Film film = new Film("Alien", "Sci-fi", 120, "Ridley Scott", premiere);
+
+            // Assert:
+            Assert.AreEqual("Ridley Scott", film.Instruktør);
+            Assert.AreEqual(premiere, film.PremiereDato);
+
+        }
+
+    }
+}
